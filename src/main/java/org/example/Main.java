a232ae0a90671c6bdb80c9ede2b6ad1acf02fdf7
BJSam <josephsamuel944@gmail.com>
2024-02-20 04:43:24 -0500

    added list of files instead of single file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         final String homePath="/home/user/Downloads/";
-        final List<String> fileNamesToCheck = Arrays.asList("file-sample_100kB.doc");
+        final List<String> fileNamesToCheck = List.of("file-sample_100kB.doc");
         final List<String> searchWords = Arrays.asList("vehicula","Java","jAva","Vestibulum");
         ReadWordFile read = new ReadWordFile();
         for(String fileName: fileNamesToCheck){

d8c2d23af87f614391c77c775ee31169f61700f7
BJSam <josephsamuel944@gmail.com>
2024-02-21 01:35:40 -0500

    grab all the doc files from given folder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,16 @@ public class Main {
 
     public static void main(String[] args) {
         final String homePath="/home/user/Downloads/";
-        final List<String> fileNamesToCheck = List.of("file-sample_100kB.doc");
         final List<String> searchWords = Arrays.asList("vehicula","Java","jAva","Vestibulum");
+        File folder = new File(homePath);
         ReadWordFile read = new ReadWordFile();
-        for(String fileName: fileNamesToCheck){
-            boolean isShortListed = read.checkForShortlisting(homePath + fileName, searchWords);
-            System.out.println("is "+fileName+" file short listed? "+isShortListed);
+        File[] listOfFiles = folder.listFiles();
+        assert listOfFiles != null;
+        for (File listOfFile : listOfFiles) {
+            if (listOfFile.isFile() && listOfFile.getName().endsWith("doc")) {
+                boolean isShortListed = read.checkForShortlisting(listOfFile.getAbsolutePath(), searchWords);
+                System.out.println("is "+listOfFile.getName()+" file short listed? "+isShortListed);
+            }
         }
     }
 }
